de8839e024ab9bd6a497ad7cd68a153a82d3a4e4
Cyryl Płotnicki <cyplo@cyplo.net>
2018-05-05 10:42:16 +0100

    codecov working locally

--- ci/upload-coverage-data.sh
+++ ci/upload-coverage-data.sh
@@ -8,16 +8,17 @@ cd target/coverage
 wget -c https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
 tar xzf master.tar.gz
 cd kcov-master
-mkdir build
+mkdir -p build
 cd build
 cmake ..
 make
 make install DESTDIR=../../kcov-build
 popd
-for file in target/debug/genpass-*[^\.d]; do
+for file in `find -wholename "./target/debug/genpass-*[^\.d]"`; do
     echo "processing $file"
     mkdir -p "target/cov/$(basename $file)"
     ./target/coverage/kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"
 done
+cd target/cov/
 bash <(curl -s https://codecov.io/bash)
 echo "Uploaded code coverage"
