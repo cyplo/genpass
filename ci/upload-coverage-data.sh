ea35e4da143c8d7dc5867a3e2d44868b5d292bd0
Cyryl Płotnicki <cyplo@cyplo.net>
2018-05-05 20:14:51 +0100

    Allow deployment when the author was not the bot

--- ci/upload-coverage-data.sh
+++ ci/upload-coverage-data.sh
@@ -3,8 +3,8 @@
 set -e
 
 pushd `pwd`
-mkdir -p target/coverage
-cd target/coverage
+mkdir -p target/kcov
+cd target/kcov
 wget -c https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
 tar xzf master.tar.gz
 cd kcov-master
@@ -16,9 +16,8 @@ make install DESTDIR=../../kcov-build
 popd
 for file in `find -wholename "./target/debug/genpass-*[^\.d]"`; do
     echo "processing $file"
-    mkdir -p "target/cov/$(basename $file)"
-    ./target/coverage/kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"
+    mkdir -p "target/coverage-data/$(basename $file)"
+    ./target/kcov/kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/coverage-data/$(basename $file)" "$file"
 done
-cd target/cov/
 bash <(curl -s https://codecov.io/bash)
 echo "Uploaded code coverage"
