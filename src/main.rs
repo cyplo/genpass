9f4ff82e57bacdce3d4d5cf2d6171ea08530a2cf
Cyryl Płotnicki <cyplo@cyplo.net>
2018-05-05 09:15:14 +0100

    Length-based generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+
+extern crate rand;
+use rand::os::OsRng;
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn generate_password(length: usize) -> String {
+    let mut rng = OsRng::new().expect("Error opening OS random number generator");
+    rng.gen_ascii_chars().take(length).collect::<String>()
+}
+
+#[cfg(test)]
+mod must_be {
+    use super::generate_password;
+
+    quickcheck! {
+        fn of_given_length(length: usize) -> bool {
+          let password = generate_password(length);
+          password.len() == length
+        }
+    }
 }
