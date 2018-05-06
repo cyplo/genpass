cd54a840448d622066b88a2f9963597569261fd0
Cyryl Płotnicki <cyplo@cyplo.net>
2018-05-06 17:06:57 +0100

    First take on supporting options

--- src/main.rs
+++ src/main.rs
@@ -20,20 +20,62 @@ fn main() {
     let length: usize = options.value_of("length").unwrap().parse().unwrap();
 
     let mut rng = OsRng::new().expect("Error opening OS random number generator");
-    println!("{}", generate_password(length, GenerationOptions::Default, &mut rng));
+    println!(
+        "{}",
+        generate_password(length, GenerationOptions::Default, &mut rng)
+    );
 }
 
-fn generate_password<Rng : rand::Rng>(length: usize, options: GenerationOptions, rng: &mut Rng) -> String {
-    rng.gen_ascii_chars().take(length).collect::<String>()
+fn generate_password<Rng: rand::Rng>(
+    length: usize,
+    options: GenerationOptions,
+    rng: &mut Rng,
+) -> String {
+    let alphabet = generate_alphabet(options);
+
+    generate_password_from_alphabet(length, rng, alphabet)
+}
+
+fn generate_alphabet(options: GenerationOptions) -> Vec<char> {
+    let lowercase = "a".chars();
+    let uppercase = "A".chars();
+    let mut alphabet = Vec::new();
+    let options_list = match options {
+        GenerationOptions::Default => vec![GenerationOption::Uppercase, GenerationOption::Lowercase],
+        GenerationOptions::Custom(list) => list
+    };
+    for option in options_list {
+        let subalphabet = match option {
+            GenerationOption::Uppercase => uppercase.clone(),
+            GenerationOption::Lowercase => lowercase.clone()
+        };
+        alphabet.extend(subalphabet);
+    }
+    alphabet
+}
+
+fn generate_password_from_alphabet<Rng: rand::Rng>(
+    length: usize,
+    rng: &mut Rng,
+    mut alphabet: Vec<char>,
+) -> String {
+    let mut password = String::with_capacity(length);
+    while password.len() < length {
+        let next_character_index = rng.next_u32() as usize % alphabet.len();
+        let next_character = alphabet[next_character_index];
+        password.push(next_character);
+    }
+    password
 }
 
 enum GenerationOption {
-    Uppercase, Lowercase
+    Uppercase,
+    Lowercase,
 }
 
 enum GenerationOptions {
     Default,
-    Custom(Vec<GenerationOption>)
+    Custom(Vec<GenerationOption>),
 }
 
 #[cfg(test)]
@@ -68,15 +110,20 @@ mod must {
         }
     }
 
-    fn test_generated_password_characters(length: usize, seed: Vec<usize>, options: GenerationOptions, predicate: &Fn(char) -> bool) -> TestResult {
+    fn test_generated_password_characters(
+        length: usize,
+        seed: Vec<usize>,
+        options: GenerationOptions,
+        predicate: &Fn(char) -> bool,
+    ) -> TestResult {
         if length == 0 || seed.len() == 0 {
-            return TestResult::discard()
+            return TestResult::discard();
         }
 
         let mut rng: StdRng = SeedableRng::from_seed(seed.as_slice());
         let password = generate_password(length, options, &mut rng);
 
-        let result = password.chars().any( predicate );
+        let result = password.chars().any(predicate);
         TestResult::from_bool(result)
     }
 }
