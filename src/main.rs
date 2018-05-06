338f9ce73216e0da66b95b534c5d510fbc3a1e91
Cyryl Płotnicki <cyplo@cyplo.net>
2018-05-06 06:51:15 +0100

    initial failing test for case support

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,80 @@
 extern crate quickcheck;
 
 extern crate rand;
-use rand::Rng;
 use rand::os::OsRng;
 
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 fn main() {
-    let options = App::new("genpass").
-        arg(Arg::with_name("length").short("l").index(1).default_value("32")).
-        get_matches();
+    let options = App::new("genpass")
+        .arg(
+            Arg::with_name("length")
+                .short("l")
+                .index(1)
+                .default_value("32"),
+        )
+        .get_matches();
     let length: usize = options.value_of("length").unwrap().parse().unwrap();
-    println!("{}", generate_password(length));
+
+    let mut rng = OsRng::new().expect("Error opening OS random number generator");
+    println!("{}", generate_password(length, GenerationOptions::Default, &mut rng));
 }
 
-fn generate_password(length: usize) -> String {
-    let mut rng = OsRng::new().expect("Error opening OS random number generator");
+fn generate_password<Rng : rand::Rng>(length: usize, options: GenerationOptions, rng: &mut Rng) -> String {
     rng.gen_ascii_chars().take(length).collect::<String>()
+}
+
+enum GenerationOption {
+    Uppercase, Lowercase
+}
+
+enum GenerationOptions {
+    Default,
+    Custom(Vec<GenerationOption>)
 }
 
 #[cfg(test)]
 mod must {
-    use super::generate_password;
+    use super::*;
+
+    use rand::StdRng;
+    use rand::SeedableRng;
+    use quickcheck::TestResult;
 
     quickcheck! {
         fn be_of_given_length(length: usize) -> bool {
-          let password = generate_password(length);
+          let mut rng = OsRng::new().unwrap();
+          let password = generate_password(length, GenerationOptions::Default, &mut rng);
           password.len() == length
         }
+
+        fn support_lowercase_letters(length: usize, seed: Vec<usize>) -> TestResult {
+          test_generated_password_characters(
+            length,
+            seed,
+            GenerationOptions::Custom(vec![GenerationOption::Lowercase]),
+            &|character| character.is_ascii_lowercase() )
+        }
+
+        fn support_uppercase_letters(length: usize, seed: Vec<usize>) -> TestResult {
+          test_generated_password_characters(
+            length,
+            seed,
+            GenerationOptions::Custom(vec![GenerationOption::Uppercase]),
+            &|character| character.is_ascii_uppercase() )
+        }
+    }
+
+    fn test_generated_password_characters(length: usize, seed: Vec<usize>, options: GenerationOptions, predicate: &Fn(char) -> bool) -> TestResult {
+        if length == 0 || seed.len() == 0 {
+            return TestResult::discard()
+        }
+
+        let mut rng: StdRng = SeedableRng::from_seed(seed.as_slice());
+        let password = generate_password(length, options, &mut rng);
+
+        let result = password.chars().any( predicate );
+        TestResult::from_bool(result)
     }
 }
