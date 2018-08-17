1c88ab83c8f6cc3ee20a32afc9afbce0eb804a39
Cyryl Płotnicki <cyplo@cyplo.net>
2018-08-17 14:41:22 +0100

    Include all dependencies compilation in the main task as to get them cached by travis

--- ci/install-publish-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ ! -x `which cargo-release` ]]; then
-    cargo install cargo-release
-fi
-
-if [[ ! -x `which cargo-tarpaulin` ]]; then
-    RUSTFLAGS="--cfg procmacro2_semver_exempt" cargo install cargo-tarpaulin
-fi
