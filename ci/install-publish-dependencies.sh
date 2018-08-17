56c1d53afc78eb094b4a0e56b4fc872ce331e553
Cyryl Płotnicki <cyplo@cyplo.net>
2018-08-17 14:20:00 +0100

    Always install tarpaulin as to include it in the cache

--- ci/install-publish-dependencies.sh
+++ ci/install-publish-dependencies.sh
@@ -3,3 +3,7 @@
 if [[ ! -x `which cargo-release` ]]; then
     cargo install cargo-release
 fi
+
+if [[ ! -x `which cargo-tarpaulin` ]]; then
+    RUSTFLAGS="--cfg procmacro2_semver_exempt" cargo install cargo-tarpaulin
+fi
--- ci/upload-coverage-data.sh
+++ ci/upload-coverage-data.sh
@@ -3,7 +3,6 @@
 set -e
 
 if [[ "$TRAVIS_RUST_VERSION" == "stable" ]]; then
-    RUSTFLAGS="--cfg procmacro2_semver_exempt" cargo install cargo-tarpaulin
     cargo tarpaulin --out Xml
     bash <(curl -s https://codecov.io/bash)
 fi
