1f2058ca39047ed537b6f1e49044e1ce5135bd43
Cyryl Płotnicki <cyplo@cyplo.net>
2019-05-04 11:37:26 +0100

    Run cargo pants first to not clobber final output

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+cargo pants
+
 cargo fmt -- --check
 cargo clippy --all-targets --all-features -- -D warnings
 cargo check
 cargo test -- --ignored
 cargo build --release
-cargo pants
 ./target/release/genpass --version
