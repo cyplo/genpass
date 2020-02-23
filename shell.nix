9dc5003eb8aab0bda125cb4f349671be900ed8ad
Cyryl Płotnicki <cyplo@cyplo.net>
2020-02-23 09:15:16 +0000

    upgrade to newer cargo release

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 let
   pkgs = import <nixpkgs> {};
+  unstableTarball = fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
+  unstable = import unstableTarball {};
 in
   pkgs.mkShell {
     buildInputs = with pkgs; [
-      rustc rustfmt cargo cargo-watch cargo-release rustPackages.clippy
+      rustc rustfmt cargo cargo-watch unstable.cargo-release rustPackages.clippy
       git direnv
       zlib
     ];
