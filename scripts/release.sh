25c8f97150ed96ee49872a116def3aa9bde04442
Cyryl Płotnicki <cyplo@cyplo.net>
2020-03-17 08:02:47 +0000

    export path

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,4 +5,5 @@ cargo install cargo-release -f
 git checkout $TRAVIS_BRANCH
 git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 cargo login $CRATES_TOKEN
+export PATH="$PATH:$HOME/.cargo/bin"
 cargo release --no-dev-version --no-confirm patch
