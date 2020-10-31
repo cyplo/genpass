e2a4537c68076c702ce56d0d7dad665b47de133c
Cyryl Płotnicki <cyplo@cyplo.net>
2020-10-31 14:03:29 +0000

    add crates login

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,10 +2,5 @@
 set -e
 
 cargo install cargo-release -f
-git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-git fetch -p
-git checkout $TRAVIS_BRANCH
-git pull --tags origin $TRAVIS_BRANCH
-cargo login $CRATES_TOKEN
 export PATH="$PATH:$HOME/.cargo/bin"
-cargo release --no-dev-version --no-confirm patch
+cargo login $CRATES_TOKEN
