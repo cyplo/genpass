d45dd4f2460f819d8a70be16353ecd2eeafb1bbb
Cyryl Płotnicki <cyplo@cyplo.net>
2020-10-31 18:15:37 +0000

    Checkout branch to release

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,4 +5,5 @@ cargo install cargo-release -f
 export PATH="$PATH:$HOME/.cargo/bin"
 git config user.email "releases@cyplo.dev"
 git config user.name "Release Bot"
+git checkout master
 cargo release --no-dev-version --no-confirm patch
