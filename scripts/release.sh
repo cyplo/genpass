9864ccc31b82dba190a5b4906765523773faf425
Cyryl Płotnicki <cyplo@cyplo.net>
2020-10-31 19:42:30 +0000

    trust sr.ht by default

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,7 @@ set -e
 
 cargo install cargo-release -f
 export PATH="$PATH:$HOME/.cargo/bin"
+ssh-keyscan git.sr.ht >> ~/.ssh/known_hosts
 git config user.email "releases@cyplo.dev"
 git config user.name "Release Bot"
 git checkout master
