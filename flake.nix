bb4af7f905b978ca1695b6e5ea6b1be66cb134c2
Cyryl Płotnicki <cyplo@cyplo.net>
2021-06-10 22:46:07 +0100

    Add basic flake

---
+++ flake.nix
@@ -0,0 +1,94 @@
+{
+  description = "A simple yet robust commandline random password generator.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crate2nix = {
+      url = "github:kolloch/crate2nix";
+      flake = false;
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, ... }:
+  let
+    name = "genpass";
+    rustChannel = "stable";
+  in
+  utils.lib.eachDefaultSystem
+  (system:
+  let
+          # Imports
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              rust-overlay.overlay
+              (self: super: {
+                # Because rust-overlay bundles multiple rust packages into one
+                # derivation, specify that mega-bundle here, so that crate2nix
+                # will use them automatically.
+                rustc = self.rust-bin.${rustChannel}.latest.default;
+                cargo = self.rust-bin.${rustChannel}.latest.default;
+              })
+            ];
+          };
+          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
+          generatedCargoNix;
+
+          # Create the cargo2nix project
+          project = pkgs.callPackage
+          (generatedCargoNix {
+            inherit name;
+            src = ./.;
+          })
+          {
+              # Individual crate overrides go here
+              # Example: https://github.com/balsoft/simple-osd-daemons/blob/6f85144934c0c1382c7a4d3a2bbb80106776e270/flake.nix#L28-L50
+              defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+                # The himalaya crate itself is overriden here. Typically we
+                # configure non-Rust dependencies (see below) here.
+                ${name} = oldAttrs: {
+                  inherit buildInputs nativeBuildInputs;
+                };
+              };
+            };
+
+            buildInputs = with pkgs; [ openssl.dev cacert openssh zlib ];
+            nativeBuildInputs = with pkgs; [ rustc cargo pkgconfig ];
+  in
+  rec {
+    packages.${name} = project.rootCrate.build;
+
+          # `nix build`
+          defaultPackage = packages.${name};
+
+          # `nix run`
+          apps.${name} = utils.lib.mkApp {
+            inherit name;
+            drv = packages.${name};
+          };
+          defaultApp = apps.${name};
+
+          # `nix develop`
+          devShell = pkgs.mkShell
+          {
+            inputsFrom = builtins.attrValues self.packages.${system};
+            buildInputs = buildInputs ++ (with pkgs;
+            [
+              nixpkgs-fmt
+              cargo-watch
+              cargo-edit
+              pkgs.rust-bin.${rustChannel}.latest.rust-analysis
+              pkgs.rust-bin.${rustChannel}.latest.rls
+            ]);
+            RUST_SRC_PATH = "${pkgs.rust-bin.${rustChannel}.latest.rust-src}/lib/rustlib/src/rust/library";
+          };
+        }
+        );
+      }
+
--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
-let
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  channel = (nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; });
-in
-  with nixpkgs;
-  stdenv.mkDerivation {
-    name = "genpass_shell";
-    buildInputs = [
-      channel.rust
-      cacert openssl openssh zlib
-      llvm pkgconfig git
-    ];
+(import
+(
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
   }
+  )
+  {
+    src = ./.;
+  }).shellNix
+
