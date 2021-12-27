85ab854b5be827bd9c435e4deb37dcbb7dc8a80d
Cyryl Płotnicki <cyplo@cyplo.net>
2021-12-27 22:20:33 +0000

    update dependencies

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A simple yet robust commandline random password generator.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
     crate2nix = {
